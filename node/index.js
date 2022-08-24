0558140bb2a3842a9178d93c65435ce2617c76d9
Bruno Andrade <brunoan99@gmail.com>
2022-08-24 04:07:50 -0300

    add express and a get route for test

--- node/index.js
+++ node/index.js
@@ -1,1 +1,12 @@
-console.log('test')
+const express = require('express')
+
+const port  = 3000
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World!</h1>')
+})
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`)
+})
