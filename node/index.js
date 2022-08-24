8f513886fd7848d33daf3037072c6eba634418b7
Bruno Andrade <brunoan99@gmail.com>
2022-08-24 04:10:55 -0300

    add docker-compose file and mysql configs to project

--- node/index.js
+++ node/index.js
@@ -4,7 +4,7 @@ const port  = 3000
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>')
+  res.send('<h1>Full Cycle Rocks!</h1>')
 })
 
 app.listen(port, () => {
